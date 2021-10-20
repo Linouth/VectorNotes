6b250734e9a3df3f4d42235e564cf9c6d921941d
Marten Trip <me@marten.xyz>
2021-10-20 22:48:52 +0200

    Initial crude implementation of bezier curve fitting

--- src/main.c
+++ src/main.c
@@ -78,6 +78,9 @@ void path_addNode(Path *path, Vec2 node) {
     path->nodes[path->node_cnt] = node;
     path->timestamps[path->node_cnt] = glfwGetTime();
 
+    printf("{%f, %f} t=%f\n", node.x, node.y, path->timestamps[path->node_cnt]);
+
+
     path->node_cnt += 1;
 }
 
@@ -87,6 +90,91 @@ Vec2* path_getNode(Path *path, int index) {
         return &path->nodes[pos];
     }
     return NULL;
+}
+
+void fitBezier(Vec2 t1, Vec2 t2, double params[], Vec2 *points, size_t count, double epsilon) {
+    Vec2 v0 = points[0];
+    Vec2 v3 = points[count-1];
+
+    double u0 = params[0];
+    double uend = params[count-1];
+
+    double c11, c1221, c22, x1, x2;
+    c11 = c1221 = c22 = x1 = x2 = 0;
+
+    for (size_t i = 0; i < count; i++) {
+        double t = (params[i] - u0) / (uend - u0);
+        Vec2 d = points[i];
+
+        double B0 = (1-t) * (1-t) * (1-t);
+        double B1 = 3*t * (1-t) * (1-t);
+        double B2 = 3*t*t * (1-t);
+        double B3 = t*t*t;
+
+        Vec2 A1 = vec2_scalarMult(t1, B1);
+        Vec2 A2 = vec2_scalarMult(t1, B2);
+
+        c11 += vec2_dot(A1, A1);
+        c1221 += vec2_dot(A1, A2);
+        c22 += vec2_dot(A2, A2);
+
+        Vec2 bisum = vec2_scalarMult(v0, B0);
+        bisum = vec2_add(bisum, vec2_scalarMult(v0, B1));
+        bisum = vec2_add(bisum, vec2_scalarMult(v3, B2));
+        bisum = vec2_add(bisum, vec2_scalarMult(v3, B3));
+
+        Vec2 sub = vec2_sub(d, bisum);
+        x1 += vec2_dot(sub, A1);
+        x2 += vec2_dot(sub, A2);
+    }
+
+    double a1 = (x1*c22 - c1221*x2) / (c11*c22 - c1221*c1221);
+    double a2 = (c11*x2 - x1*c1221) / (c11*c22 - c1221*c1221);
+
+    Vec2 v1 = vec2_add(v0, vec2_scalarMult(t1, a1));
+    Vec2 v2 = vec2_add(v3, vec2_scalarMult(t2, a2));
+
+    // TODO: Store the calculated parameters in an array for reuse.
+    double max_err = 0;
+    double max_err_t = 0;
+    for (size_t i = 0; i < count; i++) {
+        double t = (params[i] - u0) / (uend - u0);
+        Vec2 d = points[i];
+
+        double B0 = (1-t) * (1-t) * (1-t);
+        double B1 = 3*t * (1-t) * (1-t);
+        double B2 = 3*t*t * (1-t);
+        double B3 = t*t*t;
+
+        Vec2 p = vec2_scalarMult(v0, B0);
+        p = vec2_add(p, vec2_scalarMult(v1, B1));
+        p = vec2_add(p, vec2_scalarMult(v2, B2));
+        p = vec2_add(p, vec2_scalarMult(v3, B3));
+
+        double err = vec2_dist(d, p);
+        if (err > max_err) {
+            max_err = err;
+            max_err_t = t;
+        }
+
+        printf("Err at t=%f \t: %f\n", t, err);
+
+        //if (t == 0.375532) {
+        if (t >= 0.345530 && t <= 0.375535) {
+            printf(">>> orig: {%f, %f}, est: {%f, %f},\n", d.x, d.y, p.x, p.y);
+        }
+    }
+    printf("Max distance error is %f at t=%f\n", max_err, max_err_t);
+
+    printf("{%f, %f}, {%f, %f},\n", v1.x, v1.y, v2.x, v2.y);
+}
+
+void fitPath(Path *path, double epsilon) {
+    assert(path->node_cnt > 2);
+
+    Vec2 t1 = vec2_norm(vec2_sub(path->nodes[1], path->nodes[0]));
+    Vec2 t2 = vec2_norm(vec2_sub(path->nodes[path->node_cnt-1], path->nodes[path->node_cnt-2]));
+    fitBezier(t1, t2, path->timestamps, path->nodes, path->node_cnt, epsilon);
 }
 
 typedef struct ui {
@@ -263,9 +351,6 @@ static void cursor_position_callback(GLFWwindow* window, double xpos, double ypo
 
         //if (fn < 0.7 && vec2_dist(*prev_node, curr) > 8) {
         if (curr_len < (prev_len - 5.0) || vec2_dist(*prev_node, curr) > cmp) {
-            //printf("Placing new node! %f %f\n", xpos, ypos);
-            printf("{%f, %f}\n", xpos, ypos);
-
             path_addNode(&g_path, curr);
 
             prev_node = path_getNode(&g_path, -1);
@@ -303,6 +388,8 @@ static void set_viewport(int width, int height) {
 
 static void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     set_viewport(width, height);
+    ui.width = width;
+    ui.height = height;
 }
 
 static void glfwError(int id, const char* desc) {
@@ -370,36 +457,38 @@ int main(void) {
         {539.000000, 172.000000},
         {549.000000, 175.000000},
         {559.000000, 179.000000},
-        {570.000000, 183.000000},
-        {580.000000, 188.000000},
-        {591.000000, 194.000000},
-        {600.000000, 199.000000},
-        {609.000000, 205.000000},
-        {618.000000, 211.000000},
-        {626.000000, 218.000000},
-        {634.000000, 226.000000},
-        {641.000000, 234.000000},
-        {647.000000, 243.000000},
-        {652.000000, 252.000000},
-        {657.000000, 262.000000},
-        {661.000000, 273.000000},
-        {663.000000, 283.000000},
-        {665.000000, 293.000000},
-        {666.000000, 303.000000},
-        {667.000000, 313.000000},
-        {667.000000, 324.000000},
-        {667.000000, 335.000000},
-        {665.000000, 345.000000},
+        //{570.000000, 183.000000},
+        //{580.000000, 188.000000},
+        //{591.000000, 194.000000},
+        //{600.000000, 199.000000},
+        //{609.000000, 205.000000},
+        //{618.000000, 211.000000},
+        //{626.000000, 218.000000},
+        //{634.000000, 226.000000},
+        //{641.000000, 234.000000},
+        //{647.000000, 243.000000},
+        //{652.000000, 252.000000},
+        //{657.000000, 262.000000},
+        //{661.000000, 273.000000},
+        //{663.000000, 283.000000},
+        //{665.000000, 293.000000},
+        //{666.000000, 303.000000},
+        //{667.000000, 313.000000},
+        //{667.000000, 324.000000},
+        //{667.000000, 335.000000},
+        //{665.000000, 345.000000},
     };
 
     for (size_t i = 0; i < sizeof(test) / sizeof(Vec2); i++) {
         path_addNode(&g_path, test[i]);
     }
 
-    //NVGcontext *vg = nvgCreateGL3(NVG_ANTIALIAS | NVG_STENCIL_STROKES | NVG_DEBUG);
-    //if (!vg) {
-    //    return -2;
-    //}
+    fitPath(&g_path, 0.10);
+
+    NVGcontext *vg = nvgCreateGL3(NVG_ANTIALIAS | NVG_STENCIL_STROKES | NVG_DEBUG);
+    if (!vg) {
+        return -2;
+    }
 
     ui = ui_init();
 
@@ -408,11 +497,35 @@ int main(void) {
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //nvgBeginFrame(vg, WIDTH, HEIGHT, 1.0);
-        //nvgSave(vg);
+        nvgBeginFrame(vg, ui.width, ui.height, 1.0);
+        nvgSave(vg);
 
-        //nvgRestore(vg);
-        //nvgEndFrame(vg);
+        Vec2 *node = NULL;
+        node = &g_path.nodes[0];
+
+        nvgLineCap(vg, NVG_ROUND);
+        nvgLineJoin(vg, NVG_MITER);
+        nvgStrokeWidth(vg, 2.0f);
+        nvgStrokeColor(vg, nvgRGBA(82, 144, 242, 255));
+
+        nvgBeginPath(vg);
+        nvgMoveTo(vg, node->x, node->y);
+        for (size_t i = 1; i < g_path.node_cnt; i++) {
+            node = &g_path.nodes[i];
+            nvgLineTo(vg, node->x, node->y);
+        }
+        nvgStroke(vg);
+
+        nvgBeginPath(vg);
+        nvgStrokeColor(vg, nvgRGBA(230, 20, 15, 255));
+        nvgMoveTo(vg, g_path.nodes[0].x, g_path.nodes[0].y);
+        //nvgBezierTo(vg, 440.31, 199.55, 714.42, 97.9,
+        nvgBezierTo(vg, 471.4, 356, 474.0, 146.2,
+                g_path.nodes[g_path.node_cnt-1].x, g_path.nodes[g_path.node_cnt-1].y);
+        nvgStroke(vg);
+
+        nvgRestore(vg);
+        nvgEndFrame(vg);
 
         ui_drawSpline(&ui, &g_path);
 
@@ -428,6 +541,20 @@ int main(void) {
             Rgb rgb = {1.0, 1.0, 1.0};
 
             ui_drawDbgLines(&ui, points, 2, rgb);
+        }
+
+        {
+            Vec2 points[] = {
+                test[0],
+                //{444.292401, 219.462003}, {611.577620, 612.111898},
+                //{432.355162, 159.775811}, {709.610034, 121.949829},
+                {473.273050, 364.365251}, {643.173971, 212.669588},
+                test[sizeof(test)/sizeof(Vec2) - 1],
+
+                {463.000000, 219.000000}, {521.296420, 215.354138},
+            };
+            Rgb rgb = {1, 1, 1};
+            ui_drawDbgLines(&ui, points, 6, rgb);
         }
 
         // TODO: Adjacency is currently hacked in by setting the last+1 element
